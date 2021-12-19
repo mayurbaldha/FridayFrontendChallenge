a99f14ae4f3518cd2d1842200fac36c2d439d374
Mayur Baldha <82522186+mayurbaldha91@users.noreply.github.com>
2021-12-19 23:07:44 +0530

    commit data

--- frontend/my-app/src/app/store.ts
+++ frontend/my-app/src/app/store.ts
@@ -3,23 +3,23 @@ import { carsApi } from '../services/apiSlice';
 import rootReducer from './index';
 const allMiddlewares: any[] = [];
 if (process.env.NODE_ENV !== 'production') {
-  
+
 }
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware({
-    // serializableCheck: {
-    //     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    // },
-    serializableCheck: false,
-  })
-    .concat(allMiddlewares)
-    .concat(carsApi.middleware,),
-devTools: process.env.NODE_ENV !== 'production',
-// preloadedState,
-// enhancers: [reduxBatch],
-// });
+    getDefaultMiddleware({
+      // serializableCheck: {
+      //     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      // },
+      serializableCheck: false,
+    })
+      .concat(allMiddlewares)
+      .concat(carsApi.middleware,),
+  devTools: process.env.NODE_ENV !== 'production',
+  // preloadedState,
+  // enhancers: [reduxBatch],
+  // });
 });
 
 export type AppDispatch = typeof store.dispatch;
--- frontend/my-app/src/services/apiSlice.ts
+++ frontend/my-app/src/services/apiSlice.ts
@@ -3,30 +3,30 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { baseURL, FetchMethods, makesEndPoint, modelsEndPoint, vehiclesEndPoint } from '../app/constants';
 import { CommonResponse } from '../app/types';
 export const carsApi = createApi({
-  reducerPath: 'carsApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: baseURL,
-  }),
-  endpoints: (builder) => ({
-    getAllCarMakes: builder.mutation<CommonResponse,any>({
-      query: () => ({
-        url: `${makesEndPoint}`,
-        method: FetchMethods.GET,
-      }),
+    reducerPath: 'carsApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: baseURL,
     }),
-    getAllCarModels: builder.mutation<CommonResponse, any>({
-      query: ({ make }) => ({
-        url: `${modelsEndPoint}?make=${make}`,
-        method: FetchMethods.GET,
-      }),
-    }),
-    getAllVehicles: builder.mutation<CommonResponse, any>({
-        query: ({ make,model }) => ({
-          url: `${vehiclesEndPoint}?make=${make}&model=${model}`,
-          method: FetchMethods.GET,
+    endpoints: (builder) => ({
+        getAllCarMakes: builder.mutation<CommonResponse, any>({
+            query: () => ({
+                url: `${makesEndPoint}`,
+                method: FetchMethods.GET,
+            }),
         }),
-      }),
-  }),
+        getAllCarModels: builder.mutation<CommonResponse, any>({
+            query: ({ make }) => ({
+                url: `${modelsEndPoint}?make=${make}`,
+                method: FetchMethods.GET,
+            }),
+        }),
+        getAllVehicles: builder.mutation<CommonResponse, any>({
+            query: ({ make, model }) => ({
+                url: `${vehiclesEndPoint}?make=${make}&model=${model}`,
+                method: FetchMethods.GET,
+            }),
+        }),
+    }),
 });
 
 export const {
